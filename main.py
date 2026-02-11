b1a67e16e63700988b43e9db42a6dd2c87224bdb
janoch <carvajalhaddad@gmail.com>
2026-02-11 14:37:54 +1000

    final v1 - Asteroids mini project

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 import pygame
-from logger import log_state
+import sys
+from logger import log_state, log_event
 from constants import (
     SCREEN_HEIGHT,
     SCREEN_WIDTH
 )
+from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
+from shot import Shot
 
 def main():
     print("Starting Asteroids with pygame version: "+ pygame.version.ver)
@@ -12,15 +17,53 @@ def main():
 
     ## initialize game
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+
+    ##  Groups
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
+    Shot.containers = (shots, updatable, drawable)
+
+  
+    asteroid_field = AsteroidField()
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+  
+
+    ## Game Loop
 
     while True:
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-            screen.fill("black")
-            pygame.display.flip()
+        updatable.update(dt)
+        for ast in asteroids:
+            if ast.collides_with(player):
+                log_event("player_hit")
+                print("Game Over!")
+                sys.exit()
+        for ast in asteroids:
+            for shot in shots:
+                if shot.collides_with(ast):
+                    log_event("asteroid_shot")
+                    ast.split()
+                    shot.kill()
+        screen.fill("black")
+        for p in drawable:
+            p.draw(screen)
+
+        pygame.display.flip()
+        dt = clock.tick(60) / 1000
+
 
 
 if __name__ == "__main__":
